72a50c906c89be8e89b869918ff2adf384663340
lexmi09 <lexminair09@gmail.com>
2021-11-11 12:04:20 +0000

    my commit

---
+++ src/test/java/mavenpractice/Mavenjava/RESTapiTest.java
@@ -0,0 +1,20 @@
+package mavenpractice.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTapiTest {
+	
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");	
+	}
+	
+	@Test
+	public void iosapp()
+	{
+		System.out.println("iosapp");
+	}
+	
+
+}
---
+++ src/test/java/mavenpractice/Mavenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package mavenpractice.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+	System.out.println("BrowserAutomation");	
+	}
+	
+	@Test
+	public void Elements()
+	{
+		System.out.println("Elements");
+	}
+	
+
+}
